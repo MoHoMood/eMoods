281a041e6efc8d5d7e5ef7c3e6e439c76fd6477c
Ruby Sapkota <rubysapkota0@gmail.com>
2022-03-27 18:35:57 -0400

    updates

---
+++ MoHoeMoods/AccountViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AccountViewController.swift
+//  MoHoeMoods
+//
+//  Created by Ruby Sapkota on 3/27/22.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigation()
+           }
+           
+           private func setNavigation(){
+               navigationItem.title = "AccountScreen"
+           }
+       }
+
+        // Do any additional setup after loading the view.
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ MoHoeMoods/FeedViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FeedViewController.swift
+//  MoHoeMoods
+//
+//  Created by Ruby Sapkota on 3/27/22.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigation()
+           }
+           
+           private func setNavigation(){
+               navigationItem.title = "FeedScreen"
+           }
+       }
+        
+        
+        
+        
+        
+        // Do any additional setup after loading the view.
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
