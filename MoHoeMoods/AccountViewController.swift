e184dc290df84940c2dfac193f3d9e7b5d440847
Ruby Sapkota <rubysapkota0@gmail.com>
2022-03-30 19:10:06 -0400

    update

--- MoHoeMoods/AccountViewController.swift
+++ MoHoeMoods/AccountViewController.swift
@@ -25,7 +25,7 @@ class AccountViewController: UIViewController {
     
     PFUser.logOut()
     
-    let main = UIStoryboard(name: "AccountScreen", bundle:nil)
+    let main = UIStoryboard(name: "Main", bundle:nil)
     let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
     guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene , let delegate = windowScene.delegate as? SceneDelegate else {return}
     delegate.window?.rootViewController = loginViewController
