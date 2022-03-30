4588d165f2957ca44acc1edab26631ff271c930f
Ruby Sapkota <rubysapkota0@gmail.com>
2022-03-30 19:06:46 -0400

    update

--- MoHoeMoods/AccountViewController.swift
+++ MoHoeMoods/AccountViewController.swift
@@ -6,18 +6,44 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
+import MessageInputBar
+
 
 class AccountViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setNavigation()
-           }
+     
+        }
            
-           private func setNavigation(){
-               navigationItem.title = "AccountScreen"
-           }
-       }
+           
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+    
+    
+    PFUser.logOut()
+    
+    let main = UIStoryboard(name: "AccountScreen", bundle:nil)
+    let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+    guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene , let delegate = windowScene.delegate as? SceneDelegate else {return}
+    delegate.window?.rootViewController = loginViewController
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+    
+    
+    
+    
+    
+       
 
         // Do any additional setup after loading the view.
     
