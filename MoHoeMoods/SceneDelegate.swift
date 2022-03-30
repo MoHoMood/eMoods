7f9a43eca1435e4b386519340618fd768d49e492
Ruby Sapkota <rubysapkota0@gmail.com>
2022-03-30 19:22:25 -0400

    update

--- MoHoeMoods/SceneDelegate.swift
+++ MoHoeMoods/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         
         if PFUser.current() != nil {
-            let main =  UIStoryboard(name: "Main" , bundle: nil)
+            let main =  UIStoryboard(name: "FeedScreen" , bundle: nil)
             let feedNavigationController =  main.instantiateViewController(withIdentifier: "FeedNavigationController")
             window?.rootViewController = feedNavigationController
         }
