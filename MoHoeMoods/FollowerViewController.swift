03ce6f4e4aec7d8c353cbd04ecb38f24b0a44ed8
SicilyXiao <xiaoyixiao2003@gmail.com>
2022-03-30 20:30:35 -0400

    follower view in progress

---
+++ MoHoeMoods/FollowerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FollowerViewController.swift
+//  MoHoeMoods
+//
+//  Created by Yixiao Xiao on 3/30/22.
+//
+
+import UIKit
+import Parse
+
+class FollowerViewController: UIViewController {
+    
+    var followers = [PFObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
