a4f9f034dbc0f86e3d91fab0f28a3012b5496c49
SicilyXiao <xiaoyixiao2003@gmail.com>
2022-04-09 13:26:32 -0400

    follower(completed)

--- MoHoeMoods/FollowerViewController.swift
+++ MoHoeMoods/FollowerViewController.swift
@@ -14,6 +14,7 @@ class FollowerViewController: UIViewController, UITableViewDelegate, UITableView
     @IBOutlet weak var tableView: UITableView!
     
     var followers = [PFObject]()
+    var user = PFUser.current()!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +24,15 @@ class FollowerViewController: UIViewController, UITableViewDelegate, UITableView
         
        }
     
+    @IBAction func Back(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let query = PFQuery(className: "Posts")
-        query.includeKey("author")
-        query.limit = 20
-        query.order(byDescending: "createdAt")
+        let userId = user.objectId
+        let followersRelation = user.relation(forKey: "followers")
+        let query = followersRelation.query()
         
         query.findObjectsInBackground{ (followers, error) in
             if followers != nil{
@@ -36,6 +40,24 @@ class FollowerViewController: UIViewController, UITableViewDelegate, UITableView
                 self.tableView.reloadData()
             }
         }
+        
+//        let innerQuery = PFQuery(className: "User")
+//        innerQuery.whereKeyExists("username")
+//        let query = PFQuery(className: "User")
+//        query.whereKey("followers", matchesQuery: innerQuery)
+//        query.findObjectsInBackground { ( followers: [PFObject]?, error: Error?) in
+//            if let error = error {
+//                // The request failed
+//                print(error.localizedDescription)
+//            } else {
+//                // comments now contains the comments for posts with images
+//                self.followers = followers!
+//                self.tableView.reloadData()
+//            }
+//        }
+        
+//        let followers = user.value(forKey: "followers")
+//        self.tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -47,10 +69,10 @@ class FollowerViewController: UIViewController, UITableViewDelegate, UITableView
         let cell = tableView.dequeueReusableCell(withIdentifier: "FollowerCell") as! FollowerCell
         
         let follower = followers[indexPath.row]
-        let user = follower["author"] as! PFUser
-        cell.userNameLabel.text = user.username
+        let username = follower["username"] as! String
+        cell.userNameLabel.text = username
         
-        let imageFile = user["profilePhoto"] as! PFFileObject
+        let imageFile = follower["profilePhoto"] as! PFFileObject
         let urlString = imageFile.url!
         let url = URL(string: urlString)!
         
